606c521a036c17581fc6bc888e2b974ffa8d7da8
wassimbel <wassim.belhedi1@gmail.com>
2021-06-02 14:20:54 +0000

    fix betty

--- 0x04-readelf/_elf.h
+++ 0x04-readelf/_elf.h
@@ -4,7 +4,17 @@
 #include <stdlib.h>
 #include <elf.h>
 
-typedef struct
+/**
+ * struct self - elf structures
+ * @Ehdr32: ELF header structure for 32-bit architecture
+ * @Ehdr64: ELF header structure for 64-bit architecture
+ * @Phdr32: ELF program header structure for 32-bit architecture
+ * @Phdr64: ELF program header structure for 32-bit architecture
+ * @Shdr32: ELF section header structure for 32-bit architecture
+ * @Shdr64: ELF section header structure for 32-bit architecture
+ **/
+
+typedef struct elf
 {
 	Elf32_Ehdr *Ehdr32;
 	Elf64_Ehdr *Ehdr64;
@@ -15,6 +25,20 @@ typedef struct
 } elf_headers
 
 
+int check_elf(unsigned char *e_ident);
+int identify_architecture(unsigned char *e_ident);
 
+int print_magic(unsigned char *e_ident);
+int print_class(unsigned char *e_ident);
+int print_data(unsigned char *e_ident);
+int print_ei_version(unsigned char *e_ident);
+int print_osabi(unsigned char *e_ident);
+int print_entry(
+int print_entry(elf_headers *headers);
+int print_prog_headers(elf_headers *headers);
+int print_section_headers(elf_headers *headers);
+int print_flags(uint32_t e_flags);
+int print_header_size(elf_headers *headers);
+int print_prog_size(elf_headers *headers);
 
 #endif /* _ELF_H */

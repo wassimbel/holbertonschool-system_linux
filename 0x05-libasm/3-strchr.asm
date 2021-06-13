f0f13219c5b59eaa9c0fa4a486335880b1ec33a3
wassimbel <wassim.belhedi1@gmail.com>
2021-06-13 19:56:32 +0000

    strchr asm

---
+++ 0x05-libasm/3-strchr.asm
@@ -0,0 +1,28 @@
+BITS 64
+	global asm_strchr
+
+section .text
+
+asm_strchr:
+	push rbp
+	mov rbp, rsp
+
+strchr:
+	mov r8b, [rdi]
+	cmp r8b, sil
+	je str_chr
+	cmp r8b, byte 0
+	je null
+	inc rdi
+	jmp strchr
+
+str_chr:
+	mov rax, rdi
+	jmp return
+null:
+	mov rax, 0
+
+return:
+	mov rsp, rbp
+	pop rbp
+	ret

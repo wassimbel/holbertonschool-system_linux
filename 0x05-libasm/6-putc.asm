38034104aff072e638a97500478afe6c41496a9f
wassimbel <wassim.belhedi1@gmail.com>
2021-06-14 17:18:39 +0000

    fix

--- 0x05-libasm/6-putc.asm
+++ 0x05-libasm/6-putc.asm
@@ -11,20 +11,19 @@ asm_putc:
 
 	test rdi, rdi
 	jz return
-putc:
+put_c:
 	add rsp, -1
 	mov [rsp], dil
 
 	mov rax, 1
-	mov [rsi], rdi
 	mov rdi, 1
 	mov rdx, 1
+	mov rsi, rsp
 	syscall
 
 return:
 	add rsp, 1
-	pop rdx
-	pop rsi
 	mov rsp, rbp
 	pop rbp
 	ret
+

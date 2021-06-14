1f408e7e3adda6bfc9bef946ae0b68bfec78de47
wassimbel <wassim.belhedi1@gmail.com>
2021-06-14 16:55:30 +0000

    putc assembly

---
+++ 0x05-libasm/6-putc.asm
@@ -0,0 +1,34 @@
+BITS 64
+	global asm_putc
+section .text
+
+asm_putc:
+	push rbp
+	mov rbp, rsp
+	push rax
+	push rsi
+	push rdx
+
+	test rdi, rdi
+	jz return
+putc:
+	add rsp, -1
+	mov [rsp], dil
+
+	mov rax, 1
+	mov rdi, 1
+	mov rdx, 1
+	mov rsi, rsp
+	syscall
+
+
+
+
+return:
+	add rsp, 1
+	pop rdx
+	pop rsi
+	pop rdx
+	mov rsp, rbp
+	pop rbp
+	ret
--- 0x05-libasm/libasm.h
+++ 0x05-libasm/libasm.h
@@ -7,4 +7,7 @@ int asm_strncmp(const char *s1, const char *s2, size_t n);
 char *asm_strchr(const char *s, int c);
 char *asm_strstr(const char *haystack, const char *needle);
 void *asm_memcpy(void *dest, const void *src, size_t n);
+size_t asm_putc(int c);
+size_t asm_puts(const char *str);
+int asm_strcasecmp(const char *s1, const char *s2);
 #endif /*_LIBASM_H */

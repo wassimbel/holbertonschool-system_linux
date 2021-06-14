1a9b7431d8c620c8b53a25cd74fc1bc99c860a9b
wassimbel <wassim.belhedi1@gmail.com>
2021-06-14 22:31:49 +0000

    strcasecmp assembly

---
+++ 0x05-libasm/8-strcasecmp.asm
@@ -0,0 +1,57 @@
+BITS 64
+
+	global asm_strcasecmp
+	extern tolower
+
+section .text
+
+
+asm_strcasecmp:
+	push rbp
+	mov rbp, rsp
+	push rbx
+	push rcx
+	push r8
+	push r9
+	xor rcx, rcx
+	xor rax, rax
+	xor r8, r8
+	xor r9, r9
+
+strcasecmp:
+	mov r8b, [rdi + rcx]
+	mov r9b, [rsi + rcx]
+
+	cmp r8b, 65
+	jl next
+	cmp r8b, 90
+	jg next
+	add r8b, 32
+	jmp next
+
+next:
+	cmp r9b, 65
+	jl continue
+	cmp r9b, 90
+	jg continue
+	add r9b, 32
+	jmp continue
+
+continue:
+	test r8b, r8b
+	jz return
+	test r9b, r9b
+	jz return
+	inc rcx
+	cmp r8b, r9b
+	je strcasecmp
+	jmp return
+return:
+	mov rax, r8
+	sub rax, r9
+	pop r9
+	pop r8
+	pop rcx
+	mov rsp, rbp
+	pop rbp
+	ret

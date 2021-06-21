40a932726f5aa22bf3aadbd8cd32c004b008e649
wassimbel <wassim.belhedi1@gmail.com>
2021-06-21 23:51:36 +0000

    set a handler for SIGINT

---
+++ 0x06-signals/0-handle_signal.c
@@ -0,0 +1,26 @@
+#include "signals.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+
+/**
+ * handle_signal - sets a handler for the signal SIGINT
+ * Return: returns 0 on success, or -1 on error
+ **/
+
+int handle_signal(void)
+{
+	if (signal(SIGINT, handler) == SIG_ERR)
+		return (-1);
+	return (0);
+}
+
+/**
+ * handler - handler for SIGINT
+ * @signum: signal number
+ **/
+
+void handler(int signum)
+{
+	printf("Gotcha! [%d]\n", signum);
+}
---
+++ 0x06-signals/signals.h
@@ -0,0 +1,7 @@
+#ifndef SIGNALS_H_
+#define SIGNALS_H_
+
+int handle_signal(void);
+void handler(int signum);
+
+#endif /* SIGNALS_H_ */

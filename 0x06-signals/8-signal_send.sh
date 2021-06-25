8438d3ac45acc3e092843654c596a1712a02e27a
wassimbel <wassim.belhedi1@gmail.com>
2021-06-25 23:02:33 +0000

    fix argc

--- 0x06-signals/8-signal_send.sh
+++ 0x06-signals/8-signal_send.sh
@@ -1,8 +1,8 @@
 #!/bin/env sh
 # sends the signal SIGQUIT to a process, given its PID
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <pid>"
+if [ "$#" -ne 1 ]; then
+    echo 'Usage: "$0" <pid>'
     exit 1
 fi
 kill -s QUIT "$1"

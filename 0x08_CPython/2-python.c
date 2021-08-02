e6c9928993a996ba1cd3666edf735b1b60d8dd05
wassimbel <wassim.belhedi1@gmail.com>
2021-08-02 11:51:06 +0000

    new fix

--- 0x08_CPython/2-python.c
+++ 0x08_CPython/2-python.c
@@ -10,20 +10,21 @@
 void print_python_bytes(PyObject *p)
 {
 	Py_ssize_t i, size;
+	char *str;
 
 	printf("[.] bytes object info\n");
-
 	if (!PyBytes_Check(p))
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
 	}
 	size = PyBytes_Size(p);
+	str = PyBytes_AsString(p);
 	printf("  size: %ld\n", size);
-	printf("  trying string: %s\n", PyBytes_AsString(p));
-	printf("  first %ld bytes", size < 10 ? size + 1 : 10);
+	printf("  trying string: %s\n", str);
+	printf("  first %ld bytes:", size < 10 ? size + 1 : 10);
 	for (i = 0; i < 10 && i < size; i++)
-		printf(" %2hhx", PyBytes_AS_STRING(p)[i]);
+		printf(" %2hhx", str[i]);
 	printf("\n");
 }
 
@@ -36,13 +37,15 @@ void print_python_bytes(PyObject *p)
 
 void print_python_list(PyObject *p)
 {
+	Py_ssize_t i;
+
 	if (!PyList_Check(p))
 		return;
 
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", PyList_Size(p));
 	printf("[*] Allocated = %ld\n", ((PyListObject *)(p))->allocated);
-	for (Py_ssize_t i = 0; i < PyList_Size(p); i++)
+	for (i = 0; i < PyList_Size(p); i++)
 	{
 		printf("Element %ld: %s\n", i,
 		       (PyList_GET_ITEM(p, i))->ob_type->tp_name);

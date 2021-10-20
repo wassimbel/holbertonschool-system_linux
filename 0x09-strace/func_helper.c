6b850f933528de2e392f872b7194c3ba84f4f35f
wassimbel <wassim.belhedi1@gmail.com>
2021-10-20 02:33:43 +0000

    update strace

---
+++ 0x09-strace/func_helper.c
@@ -0,0 +1,68 @@
+#include "strace.h"
+
+/**
+ * str_read - returns a pointer to the string stored in address
+ * @pid: id of the process
+ * @address: address of str
+ * Return: pointer to str
+ */
+char *str_read(pid_t pid, unsigned long address)
+{
+	char *s = malloc(4096);
+	unsigned int mem = 4096;
+	unsigned long i = 0, j;
+
+	while (1)
+	{
+		if (i + sizeof(j) > mem)
+		{
+			mem *= 2;
+			s = realloc(s, mem);
+		}
+		j = ptrace(PTRACE_PEEKDATA, pid, address + i);
+		if (errno != 0)
+		{
+			s[i] = 0;
+			break;
+		}
+		memcpy(s + i, &j, sizeof(j));
+		if (memchr(&j, 0, sizeof(j)) != NULL)
+			break;
+		i += sizeof(j);
+	}
+	return (s);
+}
+
+/**
+ * print_retval - prints arguments of return value
+ * @retval: return value yscall
+ * @reg: registers struct
+ */
+void print_retval(struct user_regs_struct reg, long retval)
+{
+	char *s = ") = ";
+
+	switch (syscalls_64_g[reg.orig_rax].ret)
+	{
+	case INT:
+		fprintf(stdout, "%s%d\n", s, (int)retval);
+	case SIZE_T:
+		fprintf(stdout, "%s%lu\n", s, (ulong)retval);
+	case SSIZE_T:
+		fprintf(stdout, "%s%ld\n", s, (long)retval);
+	case LONG:
+		fprintf(stdout, "%s%ld\n", s, (long)retval);
+	case U64:
+		fprintf(stdout, "%s%lu\n", s, (ulong)retval);
+	case UINT32_T:
+		fprintf(stdout, "%s%lu\n", s, (ulong)retval);
+	case UNSIGNED_INT:
+		fprintf(stdout, "%s%u\n", s, (uint)retval);
+	case UNSIGNED_LONG:
+		fprintf(stdout, "%s%lu\n", s, (ulong)retval);
+	case PID_T:
+		fprintf(stdout, "%s%d\n", s, (int)retval);
+	default:
+		fprintf(stdout, "%s%#lx\n", s, retval);
+}
+}
--- 0x09-strace/strace.h
+++ 0x09-strace/strace.h
@@ -1,16 +1,20 @@
 #ifndef STRACE_H
 #define STRACE_H
 
+#include <errno.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/ptrace.h>
 #include <sys/reg.h>
-#include <sys/stat.h>
-#include <sys/user.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <errno.h>
+#include <sys/user.h>
+#include "syscalls.h"
 
+char *str_read(pid_t pid, unsigned long address);
+void print_retval(struct user_regs_struct reg, long retval);
 
 #endif /* STRACE_H */
